58c45068b299ef582034b6b9d746097ed60ab0a2
pruthviraj Yadav <114584082+Pruthviraj002@users.noreply.github.com>
2024-11-09 11:05:02 +0530

    upload auth

--- Route/BlogRoute.js
+++ Route/BlogRoute.js
@@ -1,12 +1,14 @@
 const {getBlog,postBlog,putBlog,deleteBlog} = require('../Controller/BlogController')
 const { login } = require('../Controller/userController')
+const Auth = require('../Middleware/Auth')
 
 const route = require("express").Router()
 
 route.get("/",getBlog)
-route.post("/",postBlog)
-route.post("/login",login)
-route.put("/:id",putBlog)
-route.delete("/:id",deleteBlog)
+route.post("/",Auth,postBlog)
+route.post("/login",Auth,login)
+
+route.put("/:id",Auth,putBlog)
+route.delete("/:id",Auth,deleteBlog)
 
 module.exports = route
